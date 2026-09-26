74da3f92207e2c05f5aaf14c9f3452aa157e0f09
Kevin D. Wolf <kevinw@software-logistics.com>
2026-09-26 08:37:11 -0400

    Add immutable media read lease contract

---
+++ src/LagoVista.MediaServices.Models/ImmutableMediaReadLease.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace LagoVista.MediaServices.Models
+{
+    public class ImmutableMediaReadLeaseRequest
+    {
+        public int? LifetimeMinutes { get; set; }
+    }
+
+    public class ImmutableMediaReadLease
+    {
+        public string Url { get; set; }
+        public DateTime ValidUntilUtc { get; set; }
+        public string MediaResourceId { get; set; }
+        public string RevisionId { get; set; }
+        public string FileName { get; set; }
+        public string ContentType { get; set; }
+        public long? ContentLength { get; set; }
+        public string ContentSha256 { get; set; }
+    }
+}
