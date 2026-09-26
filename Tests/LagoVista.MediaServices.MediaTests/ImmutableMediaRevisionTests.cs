d55267c3b111d432a7b4865015fb20796ed03efd
Kevin D. Wolf <kevinw@software-logistics.com>
2026-09-26 08:35:54 -0400

    Fix immutable lease test imports

--- Tests/LagoVista.MediaServices.MediaTests/ImmutableMediaRevisionTests.cs
+++ Tests/LagoVista.MediaServices.MediaTests/ImmutableMediaRevisionTests.cs
@@ -11,6 +11,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using System;
 using System.IO;
+using System.Linq;
 using System.Security.Cryptography;
 using System.Threading;
 using System.Threading.Tasks;
